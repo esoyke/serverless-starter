d066548ccda51511dd9b8ec841a318117a4cc284
Eric Soyke <esoyke@pscu.com>
2021-04-13 17:21:50 -0400

    try converting scss back to css to fix prerender

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Product } from 'src/app/models/Product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss'],
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   @Input() product: Product = {
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,7 +6,7 @@ import { ProductsService } from '../services/products.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
   products!: Observable<Product[]>;
